7369f3ded914e514f06db8a01013333f1979d224
Louis Y <louisy603@hotmail.com>
2022-08-24 10:01:49 -0400

    using HTTParty to fetch and scrap data with seperate endpoint

--- README.md
+++ README.md
@@ -6,17 +6,32 @@ application up and running.
 Things you may want to cover:
 
 * Ruby version
-
+    2.7.6
 * System dependencies
+    # gems used for this app. make sure to run bundle install
+    gem 'httparty'
+    
+    gem "active_model_serializers", "~> 0.10.13"
+    
+    group :development, :test do
+        <!-- # Call 'byebug' anywhere in the code to stop execution and get a debugger console -->
+        gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+    end
 
+    
 * Configuration
 
 * Database creation
+    # 3 models 
+    project
+    contributor
+    commit (join table for many to many)
 
 * Database initialization
-
+    rails db:migrate
 * How to run the test suite
-
+    run "rails s" in console to run the server
+    hit the endpoint of "localhost:3000/getProject/:owner/:repo" of the project you want to seed into the DB
 * Services (job queues, cache servers, search engines, etc.)
 
 * Deployment instructions
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,36 @@
 class ProjectsController < ApplicationController
+    def getProject
+
+        #Get project data
+        projResp = HTTParty.get("https://api.github.com/repos/#{params[:owner]}/#{params[:repo]}")
+        #using find or create by to notstore duplicates of the same project
+        proj = Project.find_or_create_by(name: projResp["name"])
+
+        #Get Project commits which also store the last users with those commits
+        commitsResp = HTTParty.get("https://api.github.com/repos/#{params[:owner]}/#{params[:repo]}/commits?per_page=10")
+        
+        #Loop through each commit to get sha to use as an endpoint to get access to nested data inside a single commit (additions/deletions/username)
+        commitsResp.each {|commit|
+            commitResp = HTTParty.get("https://api.github.com/repos/#{params[:owner]}/#{params[:repo]}/commits/#{commit["sha"]}")
+            user = Contributor.find_or_create_by(username: commitResp["author"]["login"])
+            user.commits.create(
+                sha: commitResp["sha"], 
+                additions: commitResp["stats"]["additions"], 
+                deletions: commitResp["stats"]["deletions"],
+                date: commitResp["commit"]["author"]["date"],
+                project: proj
+            )
+        }
+        render json: proj
+    end
+
+    def index
+        projects = Project.all
+        render json: projects
+    end
+
+    def show
+        project = Project.find(params[:id])
+        render json: project
+    end
 end
